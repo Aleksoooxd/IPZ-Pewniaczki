fb0acfe62485eb1cc69236cadd14beee1fed4dd3
Aleksander Frączak <fa53789>
2024-11-13 10:01:46 +0100

    dodano scraping ostatnich 5 sezonow, dokonczyc dodawanie wartosci do slownika

--- transfermarktScrap.py
+++ transfermarktScrap.py
@@ -14,43 +14,53 @@ leagues_dict = {
     'Ligue 1': "ligue-1/startseite/wettbewerb/FR1",
     'PKO BP Ekstraklasa': "pko-bp-ekstraklasa/startseite/wettbewerb/PL1"
 }
-
+season_dict = {
+    '2024/25': "/saison_id/2024",
+    '2023/24': "/saison_id/2023"
+    #'2022/23': "/saison_id/2022",
+    #'2021/22': "/saison_id/2021",
+    #'2020/21': "/saison_id/2020"
+}
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
 }
 
 
-all_clubs_data = []
+all_clubs_data = {}
 
 for league, path in leagues_dict.items():
-    url = site + path
-    print(f'Requesting URL for {league}: {url}')
-    response = requests.get(url, headers=headers)
+    for season, spath in season_dict.items():
+        url = site + path + spath
+        print(f'Requesting URL for {league}: {url}')
+        response = requests.get(url, headers=headers)
 
     # Sprawdzenie odpowiedzi serwera
-    if response.status_code == 200:
-        print(f"Success for {league}")
-    else:
-        raise Exception(f"Error response for {league}: {response.status_code}")
+        if response.status_code == 200:
+            print(f"Success for {league}")
+        else:
+            raise Exception(f"Error response for {league}: {response.status_code}")
 
     # Parsowanie treści strony
-    soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, 'html.parser')
 
 
-    club_names = soup.find_all('td', class_='hauptlink no-border-links')
+        club_names = soup.find_all('td', class_='hauptlink no-border-links')
 
 
-    club_values = soup.find_all('td', class_='rechts')
+        club_values = soup.find_all('td', class_='rechts')
 
-    print(f"Clubs in {league}:")
-    for i, name_td in enumerate(club_names):
+        print(f"Clubs in {league}:")
+        for i, name_td in enumerate(club_names):
 
-        club_name = name_td.text.strip()
-        club_value = club_values[i].text.strip()
+            club_name = name_td.text.strip()
+            club_value = club_values[((i + 1) * 2) + 1].text.strip()
+            if club_name in all_clubs_data:
+                all_clubs_data.update({club_name: club_value})
+            else:
+                all_clubs_data[club_name] = club_value
 
-        all_clubs_data.append([club_name, club_value])
-        print(f"- {club_name}: {club_value}")
+            print(f"- {club_name}: {club_value}")
 
 
 
