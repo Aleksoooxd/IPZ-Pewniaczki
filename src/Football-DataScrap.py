3c9e37903f1dfc1896af97f6c1a140001fb3c381
bialy02 <kacperbi2002@gmail.com>
2024-11-25 17:42:31 +0100

    Dodano pobieranie pozostalych sezonow (do 2000/2001) poprawiono struktore plikow w Data/

--- src/Football-DataScrap.py
+++ src/Football-DataScrap.py
@@ -3,20 +3,42 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import datetime
-#countries = ['england','spain','germany','xsds' ,'italy','france']
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
 }
+
+
 curr_year = datetime.datetime.now().year
 curr_year_2_digits = curr_year % 100
-seasons = {
-    f'{curr_year}/{curr_year+1}': f'{curr_year_2_digits}{curr_year_2_digits+1}',
-    f'{curr_year-1}/{curr_year}': f'{curr_year_2_digits-1}{curr_year_2_digits}',
-    f'{curr_year-2}/{curr_year-1}': f'{curr_year_2_digits-2}{curr_year_2_digits-1}',
-    f'{curr_year-3}/{curr_year-2}': f'{curr_year_2_digits-3}{curr_year_2_digits-2}',
-    f'{curr_year-4}/{curr_year-3}': f'{curr_year_2_digits-4}{curr_year_2_digits-3}'
-}
 
+seasons = {
+    f'{curr_year}/{curr_year+1}': f'{curr_year_2_digits}{curr_year_2_digits+1}', #24/25 (current season)
+    f'{curr_year-1}/{curr_year}': f'{curr_year_2_digits-1}{curr_year_2_digits}', #23/24
+    f'{curr_year-2}/{curr_year-1}': f'{curr_year_2_digits-2}{curr_year_2_digits-1}', #22/23
+    f'{curr_year-3}/{curr_year-2}': f'{curr_year_2_digits-3}{curr_year_2_digits-2}', #21/22
+    f'{curr_year-4}/{curr_year-3}': f'{curr_year_2_digits-4}{curr_year_2_digits-3}', #20/21
+    f'{curr_year-5}/{curr_year-4}': f'{curr_year_2_digits-5}{curr_year_2_digits-4}', #19/20
+    f'{curr_year-6}/{curr_year-5}': f'{curr_year_2_digits-6}{curr_year_2_digits-5}', #18/19
+    f'{curr_year-7}/{curr_year-6}': f'{curr_year_2_digits-7}{curr_year_2_digits-6}', #17/18
+    f'{curr_year-8}/{curr_year-7}': f'{curr_year_2_digits-8}{curr_year_2_digits-7}', #16/17
+    f'{curr_year-9}/{curr_year-8}': f'{curr_year_2_digits-9}{curr_year_2_digits-8}', #15/16
+    f'{curr_year-10}/{curr_year-9}': f'{curr_year_2_digits-10}{curr_year_2_digits-9}', #14/15
+    f'{curr_year-11}/{curr_year-10}': f'{curr_year_2_digits-11}{curr_year_2_digits-10}', #13/14
+    f'{curr_year-12}/{curr_year-11}': f'{curr_year_2_digits-12}{curr_year_2_digits-11}', #12/13
+    f'{curr_year-13}/{curr_year-12}': f'{curr_year_2_digits-13}{curr_year_2_digits-12}', #11/12
+    f'{curr_year-14}/{curr_year-13}': f'{curr_year_2_digits-14}{curr_year_2_digits-13}', #10/11
+    f'{curr_year-15}/{curr_year-14}': f'0{curr_year_2_digits-15}{curr_year_2_digits-14}', #9/10
+    f'{curr_year-16}/{curr_year-15}': f'0{curr_year_2_digits-16}0{curr_year_2_digits-15}', #8/9
+    f'{curr_year-17}/{curr_year-16}': f'0{curr_year_2_digits-17}0{curr_year_2_digits-16}', #7/8
+    f'{curr_year-18}/{curr_year-17}': f'0{curr_year_2_digits-18}0{curr_year_2_digits-17}', #6/7
+    f'{curr_year-19}/{curr_year-18}': f'0{curr_year_2_digits-19}0{curr_year_2_digits-18}', #5/6
+    f'{curr_year-20}/{curr_year-19}': f'0{curr_year_2_digits-20}0{curr_year_2_digits-19}', #4/5
+    f'{curr_year-21}/{curr_year-20}': f'0{curr_year_2_digits-21}0{curr_year_2_digits-20}', #3/4
+    f'{curr_year-22}/{curr_year-21}': f'0{curr_year_2_digits-22}0{curr_year_2_digits-21}', #2/3
+    f'{curr_year-23}/{curr_year-22}': f'0{curr_year_2_digits-23}0{curr_year_2_digits-22}', #1/2
+    f'{curr_year-24}/{curr_year-23}': f'0{curr_year_2_digits-24}0{curr_year_2_digits-23}', #0/1
+}
 
 
 countries = {
@@ -51,7 +73,7 @@ def get_data_from_top_5(countryInfo, seesonCode):
 
      if download_response.status_code != 200:
          print('Failed to load page {}'.format(download_url))
-     filepath = '../Data/Matches Results/'
+     filepath = f'../Data/Matches Results/{countryInfo[0]}/'
      filename = f'{countryInfo[1].replace(" ","")}_{seesonCode}' + '.csv'
      fullname = os.path.join(filepath, filename)
      with open(fullname, 'wb') as file:
@@ -75,7 +97,7 @@ def get_data_from_poland():
     if dowload_response.status_code != 200:
         print('Failed to load page {}'.format(dowload_url))
 
-    filepath = '../Data/Matches Results/'
+    filepath = '../Data/Matches Results/poland/'
     filename = 'Ekstraklasa_allSeasons' + '.csv'
     with open((filepath + filename), 'wb') as file:
         file.write(dowload_response.content)
@@ -85,8 +107,8 @@ def get_data_from_poland():
     df.to_csv(filepath + filename, index=False)
     print(f"Plik został pobrany i zapisany! sciezka: {filepath + filename}")
 
-#for key, countryValues in countries.items():
-#    for key, seasonsCode in seasons.items():
-#        get_data_from_top_5(countryValues, seasonsCode)
+for key, countryValues in countries.items():
+    for key, seasonsCode in seasons.items():
+        get_data_from_top_5(countryValues, seasonsCode)
 
 get_data_from_poland()
