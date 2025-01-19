5f24ef145b6d28b5165963e275e64419621fe394
fa53789 <fa53789@zut.edu.pl>
2025-01-19 18:57:21 +0100

    boze...

--- src/runBiDaily.py
+++ src/runBiDaily.py
@@ -4,13 +4,13 @@ from transfermarktScrap import scrape_transfermarkt
 from helpfunctions import reset_data
 def main():
     reset_data()
-    scrape_top_11()
-    scrape_transfermarkt()
-    file_op = FileOperator()
-    file_op.generate_seasons_with_values()
-    file_op.count_collumns()
-    file_op.generate_all_bookmakers()
-    file_op.calculate_placements()
-    file_op.add_statistics_and_consensus()
+    # scrape_top_11()
+    # scrape_transfermarkt()
+    # file_op = FileOperator()
+    # file_op.generate_seasons_with_values()
+    # file_op.count_collumns()
+    # file_op.generate_all_bookmakers()
+    # file_op.calculate_placements()
+    # file_op.add_statistics_and_consensus()
 if __name__ == "__main__":
     main()
