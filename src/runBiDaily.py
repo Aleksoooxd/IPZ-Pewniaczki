e2810e2747acbedee5d0ffd8551ec8e6e3053918
fa53789 <fa53789@zut.edu.pl>
2025-02-19 21:15:08 +0100

    trzeba ulepszyc bo dziala wolno + nie znajduje niektorych meczow przez brak jednolitosci nazw :( wiec nw czy to ma przyszlosc

--- src/runBiDaily.py
+++ src/runBiDaily.py
@@ -12,5 +12,6 @@ def main():
     file_op.generate_all_bookmakers()
     file_op.calculate_placements()
     file_op.add_statistics_and_consensus()
+    file_op.final_merge()
 if __name__ == "__main__":
     main()
--- src/test.py
+++ src/test.py
@@ -2,11 +2,13 @@ from datetime import datetime
 import asyncio
 import json
 import httpx
+import pandas as pd
+import os
 import requests
 import time
 def team_modifier(teamh,teama,separator,use_date=False,date="20/09/2005"):
     if use_date:
-        date = date[-4:]
+        date = date[:4]
     else:
         date=""
     link = teamh+" "+teama+" "+date
@@ -19,32 +21,70 @@ async def find_match_link(home_team, away_team, match_date):
     async with httpx.AsyncClient() as client:
         response = await client.get(base_url)
     if response.status_code != 200:
-        print(f"Błąd zapytania: {response.status_code}")
+        #print(f"Błąd zapytania: {response.status_code}")
         return None
     data = response.json()
-    print(home_team,away_team,match_date)
     for i,entity in enumerate(data['results']):
         entity = entity['entity']
         ht = entity['homeTeam']['name']
         at = entity['awayTeam']['name']
-        date = datetime.fromtimestamp(entity['startTimestamp']).strftime("%d/%m/%Y")
+        date = datetime.fromtimestamp(entity['startTimestamp']).strftime("%Y-%m-%d")
         if date.replace(" ", "") != match_date.replace(" ", ""):
             continue
-        customid = entity['customId']
+        #customid = entity['customId']
         id = entity['id']
-        teams_link = team_modifier(ht,at,"-")
-        match_link = f"https://www.sofascore.com/football/match/{teams_link}/{customid}#id:{id}"
+        #teams_link = team_modifier(ht,at,"-")
+        #match_link = f"https://www.sofascore.com/football/match/{teams_link}/{customid}#id:{id}"
+        #print(match_link)
         lineups_link = f"https://www.sofascore.com/api/v1/event/{id}/lineups"
-        print(lineups_link)
-        statistics_link = f"https://www.sofascore.com/api/v1/event/{id}/statistics"
-        print(statistics_link)
-        managers_link = f"https://www.sofascore.com/api/v1//event/{id}/managers"
-        return match_link
-
+        #print(lineups_link)
+        #statistics_link = f"https://www.sofascore.com/api/v1/event/{id}/statistics"
+        #print(statistics_link)
+        #managers_link = f"https://www.sofascore.com/api/v1//event/{id}/managers"
+        return lineups_link
+async def extract_lineups(base_url):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(base_url)
+    if response.status_code != 200:
+        #print(f"Błąd zapytania: {response.status_code}")
+        return None,None
+    data = response.json()
+    homeplayers = data["home"]["players"]
+    awayplayers = data["away"]["players"]
+    homep = []
+    awayp =[]
+    for player in homeplayers:
+        homep.append(player["player"]["name"])
+    for player in awayplayers:
+        awayp.append(player["player"]["name"])
+    homep = homep[:11]
+    awayp = awayp[:11]
+    return homep,awayp
 async def main():# Przykład użycia
-    home_team = "Pogon Szczecin"
-    away_team = "Zaglebie Lubin"
-    match_date = "04/12/2024"
-    match_link = await find_match_link(home_team, away_team, match_date)
-    print(match_link)
+    filepath = os.path.join("..",'Data', 'FinalData', 'AllBookmakers','AllLeagues.csv')
+    df = pd.read_csv(filepath)
+    lineup_columns = [f"H{i}" for i in range(1, 12)] + [f"A{i}" for i in range(1, 12)]
+    for col in lineup_columns:
+        df[col] = None
+    for index, row in df.iterrows():
+        if index > 4000:
+            home_team = row['HomeTeam']
+            away_team = row['AwayTeam']
+            match_date = str(row['Date'])
+            lineups_link = await find_match_link(home_team, away_team, match_date)
+
+            if lineups_link is None:
+                print(f"Match {index}, " f"HT: {home_team}", " ", f"AT: {away_team}", "failed - no match link")
+                continue
+            home_lineup, away_lineup = await extract_lineups(lineups_link)
+            if home_lineup is None or away_lineup is None:
+                print(f"Match {index}, " f"HT: {home_team}", " ", f"AT: {away_team}", "failed - no lineups link")
+                continue
+            lineups = home_lineup + away_lineup
+            for ind,col in enumerate(lineup_columns):
+                df.at[index, col] = lineups[ind]
+            #df.at[index, lineup_columns[:11]] = home_lineup
+            #df.at[index, lineup_columns[11:]] = away_lineup
+            print(f"Match {index}, " f"HT: {home_team}", " ", f"AT: {away_team}", "processed")
+    df.to_csv(filepath,index=False)
 asyncio.run(main())
