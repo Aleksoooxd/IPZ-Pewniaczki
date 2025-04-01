fdf79b76b8ce378a48c042cd61c9686b8de4a479
bialy02 <kacperbi2002@gmail.com>
2025-04-01 18:54:37 +0200

    dodanie routow do lig

--- src/flask/main.py
+++ src/flask/main.py
@@ -13,5 +13,40 @@ def premierleague():
 def bundesliga():
     return render_template('bundesliga.html')
 
+@app.route('/laliga')
+def laliga():
+    return render_template('laliga.html')
+
+@app.route('/ligue1')
+def ligue1():
+    return render_template('ligue1.html')
+
+@app.route('/seriea')
+def seriea():
+    return render_template('seriea.html')
+
+@app.route('/eredivisie')
+def eredivisie():
+    return render_template('eredivisie.html')
+
+@app.route('/scotishpremierleague')
+def scotishpremierleague():
+    return render_template('scotishpremierleague.html')
+
+@app.route('/greecesuperleague')
+def greecesuperleague():
+    return render_template('greecesuperleague.html')
+@app.route('/jupilerleague')
+def jupilerleague():
+    return render_template('jupilerleague.html')
+
+@app.route('/superleauge')
+def superleauge():
+    return render_template('superleauge.html')
+
+@app.route('/leaugeportugal')
+def leaugeportugal():
+    return render_template('leaugeportugal.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
