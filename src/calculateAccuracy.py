418c1dc29bf5725b56b266e6f28bd536e738eede
fa53789 <fa53789@zut.edu.pl>
2025-06-04 01:46:20 +0200

    srednia pewnosc predykcji w prawidlowej predykcji

--- src/calculateAccuracy.py
+++ src/calculateAccuracy.py
@@ -1,20 +1,20 @@
 import pandas as pd
 from sqlalchemy import create_engine, text
-from flask_app.app.db import db, app, FootballMatch, Predicted, MatchStats # Assuming db and app are correctly imported from your Flask app
+from flask_app.app.db import db, app, FootballMatch, Predicted, MatchStats
 
-def calculate_prediction_accuracy_and_odds():
+def calculate_prediction_accuracy_odds_confidence():
     with app.app_context():
         football_matches = db.session.query(FootballMatch).filter(FootballMatch.result.isnot(None)).all()
         df_matches = pd.DataFrame([match.__dict__ for match in football_matches])
-        df_matches = df_matches.drop(columns=['_sa_instance_state']) # Drop SQLAlchemy internal state column
+        df_matches = df_matches.drop(columns=['_sa_instance_state'])
 
         predictions = db.session.query(Predicted).all()
         df_predictions = pd.DataFrame([pred.__dict__ for pred in predictions])
-        df_predictions = df_predictions.drop(columns=['_sa_instance_state']) # Drop SQLAlchemy internal state column
+        df_predictions = df_predictions.drop(columns=['_sa_instance_state'])
 
         match_stats = db.session.query(MatchStats).all()
         df_match_stats = pd.DataFrame([stats.__dict__ for stats in match_stats])
-        df_match_stats = df_match_stats.drop(columns=['_sa_instance_state']) # Drop SQLAlchemy internal state column
+        df_match_stats = df_match_stats.drop(columns=['_sa_instance_state'])
 
         if df_matches.empty:
             print("No finished matches found in the database.")
@@ -25,7 +25,8 @@ def calculate_prediction_accuracy_and_odds():
         if df_match_stats.empty:
             print("No match statistics found in the database.")
             return
-        df_merged = pd.merge(df_matches[['match_id', 'result']], df_predictions[['match_id', 'predicted_result']], on='match_id', how='inner')
+
+        df_merged = pd.merge(df_matches[['match_id', 'result']], df_predictions[['match_id', 'predicted_result', 'confidence']], on='match_id', how='inner')
 
         if df_merged.empty:
             print("No common matches between finished matches and predictions. Cannot calculate accuracy.")
@@ -61,15 +62,20 @@ def calculate_prediction_accuracy_and_odds():
                 print(f"Average odds for correctly predicted outcomes: {average_odds_for_correct_predictions:.2f}")
             else:
                 print("No valid odds data found for correctly predicted outcomes to calculate average.")
+
+            average_confidence_for_correct_predictions = correct_predictions_df['confidence'].mean()
+            print(f"Average confidence for correctly predicted outcomes: {average_confidence_for_correct_predictions:.2f}")
+
         else:
-            print("No correct predictions to calculate average odds for.")
+            print("No correct predictions to calculate average odds or confidence for.")
 
 if __name__ == "__main__":
-    calculate_prediction_accuracy_and_odds()
-#Total predictions considered: 71883
-#Correct predictions: 61073
-#Accuracy: 84.96%
-#Average odds for correctly predicted outcomes: 1.86
+    calculate_prediction_accuracy_odds_confidence()
+# Total predictions considered: 71883
+# Correct predictions: 61073
+# Accuracy: 84.96%
+# Average odds for correctly predicted outcomes: 1.86
+# Average confidence for correctly predicted outcomes: 0.86
 # 30 dni 1 zaklad dziennie o wartosci np 10 zl
 # 30* 84.96% ~= 25
 # 25 * 10zl * 1.86 ~= 465 zl
